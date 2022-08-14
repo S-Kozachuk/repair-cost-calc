60025e22f67dd587e1601dcbd199eefbadf3c7f0
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-08-14 10:58:48 +0300

    Added all inputs in collection

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const squareInput = document.querySelector('#square-input');
 const squareRange = document.querySelector('#square-range');
 const basePrice = 6000;
+const inputs = document.querySelectorAll('input');
 let totalPrice;
+
 
 squareRange.addEventListener('input', function() {
 	console.log('Input');
