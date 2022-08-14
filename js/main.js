4a2ccc86498b7179abab3a49bffa303bdb4adf3a
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-08-14 16:15:33 +0300

    Adde radiobtn as repairType variable(colect)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+const inputs = document.querySelectorAll('input');
 const squareInput = document.querySelector('#square-input');
 const squareRange = document.querySelector('#square-range');
+const repairType = document.querySelectorAll('input[name="type"]');
 const basePrice = 6000;
-const inputs = document.querySelectorAll('input');
 let totalPrice = document.getElementById('total-price');
 
 squareRange.addEventListener('input', function() {
@@ -15,6 +16,8 @@ squareInput.addEventListener('input', ()=>{
 function calculate() {
 	let calcPrice = basePrice * parseInt(squareInput.value);
 	console.log(calcPrice);
+	const formatter = new Intl.NumberFormat('ru');
+	totalPrice.innerText = formatter.format(calcPrice);
 };
 calculate();
 
