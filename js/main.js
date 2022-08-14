a62ea9b7d4a64fa0e317750434c98b84f3c0f8c1
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-08-14 11:16:24 +0300

    Wrote a calculate()

--- js/main.js
+++ js/main.js
@@ -2,19 +2,23 @@ const squareInput = document.querySelector('#square-input');
 const squareRange = document.querySelector('#square-range');
 const basePrice = 6000;
 const inputs = document.querySelectorAll('input');
-let totalPrice;
-
 
 squareRange.addEventListener('input', function() {
-	console.log('Input');
 	squareInput.value = squareRange.value;
-})
+});
 
 squareInput.addEventListener('input', ()=>{
 	squareRange.value = squareInput.value; 
-})
+});
 
-totalPrice = basePrice * parseInt(squareInput.value);
-console.log(totalPrice);
-console.log(squareInput.value)
-console.log(typeof(squareInput.value))
+let calculate = (()=>{
+	let totalPrice = basePrice * parseInt(squareInput.value);
+	console.log(totalPrice)
+});
+calculate()
+
+for (const item of inputs) {
+	item.addEventListener('input',()=>{
+		calculate();
+	})
+}
