44242d8bbe287c77b3134b690841fe886fbe4900
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-08-14 08:55:15 +0300

    Linked the input fields of the square

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ const squareInput = document.querySelector('#square-input');
 const squareRange = document.querySelector('#square-range');
 
 squareRange.addEventListener('input', function() {
-	console.log('Input'); 
+	console.log('Input');
+	squareInput.value = squareRange.value;
+})
+
+squareInput.addEventListener('input', ()=>{
+	squareRange.value = squareInput.value; 
 })
